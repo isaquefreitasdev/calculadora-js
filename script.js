e774c7f73d1b9485a869e1b49002d7e43725838d
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-01-26 10:42:46 -0300

    Nova função

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let n1 = document.getElementById('value1')
 let n2 = document.getElementById('value2')
-
-
+let char = ["ABCDEFGHIJKLMNOPQRSTUVWXY",]
+function verificastring(){
+    if(n1.value.search("ABCDEFGHIJKLMNOPQRSTUVWXa - b - c- d - e- f - g - h - i - j - k - l - m - n - o - p - q - r - s - t - u - v - w - x - y - z.")){
+        alert('Somente números')
+    }
+}
 let soma = () => {
     let calc = Number(parseInt(n1.value) + parseInt(n2.value))
+    verificastring()
     if(n1.value == "" || n2.value === ""){
         alert("Preencha todos os campos")
     }else{
